f77e569228a0f2d770f9ce703a3c8f88b46d9071
Anton374-k <anton.kozoriz@knu.ua>
2025-05-26 23:22:26 +0300

    utils2.py

--- game_files/utils.py
+++ game_files/utils.py
@@ -8,3 +8,8 @@ def load_image(image_path, new_size=None):
     if new_size:
         image = pygame.transform.scale(image, new_size)
     return image
+
+def create_car(x, y, car_image):
+    """Створити об'єкт машини"""
+    car = pygame.Rect(x, y, car_image.get_width(), car_image.get_height())
+    return car
